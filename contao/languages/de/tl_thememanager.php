af56828f041e1466a5a27a7b08401e81f82837f8
Samuel Heer <samuel@ex-akt.de>
2025-01-27 14:26:20 +0100

    progress

---
+++ contao/languages/de/tl_thememanager.php
@@ -0,0 +1,8 @@
+<?php 
+
+declare(strict_types=1);
+$GLOBALS['TL_LANG']['tl_thememanager']['isotope_header'] = "Erweiterung: Isotope Shop";
+$GLOBALS['TL_LANG']['tl_thememanager']['isotope_detail_legend'] = "Produktdetail";
+$GLOBALS['TL_LANG']['tl_thememanager']['isotope_checkout_legend'] = "Checkout";
+$GLOBALS['TL_LANG']['tl_thememanager']['iso-detail-price-fs'] = array("\$iso-detail-price-fs","Schriftgröße Preis");
+$GLOBALS['TL_LANG']['tl_thememanager']['iso-detail-price-fw'] = array("\$iso-detail-price-fw","Schriftstärke Preis");
